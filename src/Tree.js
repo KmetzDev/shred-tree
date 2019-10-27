ed570ac9c4e51878f930b66078e15e22b316640a
Alex Kmetz <kmetzdevelopment@gmail.com>
2019-10-27 10:24:13 -0400

    Initial Commit

---
+++ src/Tree.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Tree() {
+    return (
+        <div>
+            <h1>Tree Goes Here</h1>
+        </div>
+    );
+}
+
+export default Tree;
---
+++ src/Trick.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Trick() {
+    return (
+        <div>
+            <h1>Trick Name: </h1>
+            <h3>Difficulty: </h3>
+            <h3>Required: </h3>
+            <h3>Leads To: </h3>
+        </div>
+    );
+}
+
+export default Trick;
